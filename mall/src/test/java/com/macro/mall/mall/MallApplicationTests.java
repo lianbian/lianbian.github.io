87c588705f30fea0eaab03e5e49a9ab6fce70730
chengbin <php_er@126.com>
2021-02-15 09:04:22 +0800

    mall项目

---
+++ mall/src/test/java/com/macro/mall/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macro.mall.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
