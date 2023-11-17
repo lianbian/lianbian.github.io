43b0893a185a8dd177c8eec1a7bd93e992c24237
lianbian <ilianbian@163.com>
2023-11-17 16:20:11 +0800

    博客文章发布

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-Hexo g && curl -H 'Content-Type:text/plain' --data-binary @public/urls.txt "http://data.zz.baidu.com/urls?site=https://www.lianbian.net&token=qrt0ct8hhTWG9pnP" && git add . && git commit -m "博客文章发布" && git push origin && ./qshell qupload upload.conf
+hexo g && curl -H 'Content-Type:text/plain' --data-binary @public/urls.txt "http://data.zz.baidu.com/urls?site=https://www.lianbian.net&token=qrt0ct8hhTWG9pnP" && git add . && git commit -m "博客文章发布" && git push origin && ./qshell qupload upload.conf
