40ad35dad2379c60709cecd84509f99061188e49
lianbian <ilianbian@163.com>
2025-01-05 09:39:37 +0800

    博客文章发布

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,13 @@ git add .
 git commit -m "博客文章发布"
 git push origin
 git push deploy
+
+# 为生成页面创建单独分支
+cd ./public && git init -b lianbian-pages && git add .
+git config user.name "lianbian"
+git config user.email "ilianbian@163.com"
+git add .
+git commit -m "Builder at $(date +'%Y-%m-%d %H:%M:%S')"
+git push --force --quiet "root@139.224.69.35:/root/www.lianbian.net.git" lianbian-pages:lianbian-pages
+
+git push deploy
