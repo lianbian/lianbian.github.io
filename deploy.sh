928b0653094fc62aecb3ebfe4396d2b85bb0e2d6
lianbian <ilianbian@163.com>
2025-01-05 09:43:19 +0800

    博客文章发布

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@ curl -H 'Content-Type:text/plain' --data-binary @public/urls.txt "http://data.zz
 git add .
 git commit -m "博客文章发布"
 git push origin
-git push deploy
 
 # 为生成页面创建单独分支
 cd ./public && git init -b lianbian-pages && git add .
@@ -12,6 +11,6 @@ git config user.name "lianbian"
 git config user.email "ilianbian@163.com"
 git add .
 git commit -m "Builder at $(date +'%Y-%m-%d %H:%M:%S')"
-git push --force --quiet "root@139.224.69.35:/root/www.lianbian.net.git" lianbian-pages:lianbian-pages
+git push deploy lianbian-pages:lianbian-pages
 
 git push deploy
