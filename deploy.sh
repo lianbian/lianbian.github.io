2bb15ad9c711f5087aa2230015e145e3cc31733f
lianbian <ilianbian@163.com>
2025-01-07 19:54:45 +0800

    博客文章发布

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 # hexo clean
 hexo generate
-curl -H 'Content-Type:text/plain' --data-binary @public/urls.txt "http://data.zz.baidu.com/urls?site=https://www.lianbian.net&token=qrt0ct8hhTWG9pnP"
 git add .
 git commit -m "博客文章发布"
 git push origin
